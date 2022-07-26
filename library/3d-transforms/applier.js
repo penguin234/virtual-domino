f9c8062a4e376397f12ba7115b804c3e60a80f26
penguin234 <56291718+penguin234@users.noreply.github.com>
2022-07-26 15:41:32 +0900

    Added unit testing form

--- library/3d-transforms/applier.js
+++ library/3d-transforms/applier.js
@@ -1,17 +1,32 @@
 Applier = {};
 
-Applier.apply = function(vector, transform) {
-    obj = [vector.x, vector.y, vector.z, 1];
+Applier.apply = function(target, transform) {
     res = [0, 0, 0, 0];
 
     for (const colindex in res) {
         for (const k in transform.matrix[colindex]) {
-            res[colindex] += obj[k] * transform.matrix[k][colindex];
+            res[colindex] += target[k] * transform.matrix[k][colindex];
         }
     }
 
-    return new Vector3D(res[0], res[1], res[2]);
+    return res;
 };
+
+Applier.toVector = function(vector, transform) {
+    const obj = [vector.x, vector.y, vector.z, 0];
+
+    const res = Applier.apply(obj, transform);
+
+    return new Vector3D(res[0], res[1], res[2]);
+}
+
+Applier.toPoint = function(vector, transform) {
+    const obj = [vector.x, vector.y, vector.z, 1];
+
+    const res = Applier.apply(obj, transform);
+
+    return new Vector3D(res[0], res[1], res[2]);
+}
 
 Applier.merge = function(transform1, transform2) {
     return Matrix4By4.prod(transform1, transform2);
---
+++ library/camera/perspectivecamera.js
@@ -0,0 +1,6 @@
+function PerspectiveCamera(position, lookdirection, updirection, horizontalangle) {
+    this.position = position;
+    this.lookdirection = lookdirection;
+    this.updirection = updirection;
+    this.horizontalangle = horizontalangle;
+}
