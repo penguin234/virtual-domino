971f04eeb51d37cb4c47c1f4c3b36805e51597e3
penguin234 <56291718+penguin234@users.noreply.github.com>
2022-07-27 17:19:25 +0900

    Added 3by3 matrix and fixed 4by3 matrix

--- library/3d-elements/matrix3by3.js
+++ library/3d-elements/matrix3by3.js
@@ -1,44 +1,40 @@
-function Matrix4By3(
+function Matrix3By3(
     n00, n01, n02,
     n10, n11, n12,
-    n20, n21, n22,
-    n30, n31, n32
+    n20, n21, n22
     ) {
     this.matrix = [
         [n00, n01, n02],
         [n10, n11, n12],
-        [n20, n21, n22],
-        [n30, n31, n32]
+        [n20, n21, n22]
     ];
 }
 
-Matrix4By3.fromArray = function(matrix) {
+Matrix3By3.fromArray = function(matrix) {
     return new Matrix4By4(
         matrix[0][0], matrix[0][1], matrix[0][2],
         matrix[1][0], matrix[1][1], matrix[1][2],
-        matrix[2][0], matrix[2][1], matrix[2][2],
-        matrix[3][0], matrix[3][1], matrix[3][2]
+        matrix[2][0], matrix[2][1], matrix[2][2]
     );
 };
 
-Matrix4By3.prototype.__proto__ = Matrix.prototype;
+Matrix3By3.prototype.__proto__ = Matrix.prototype;
 
-Matrix4By3.prototype.copy = function() {
-    return new Matrix4By3(
+Matrix3By3.prototype.copy = function() {
+    return new Matrix3By3(
         this.matrix[0][0], this.matrix[0][1], this.matrix[0][2],
         this.matrix[1][0], this.matrix[1][1], this.matrix[1][2],
-        this.matrix[2][0], this.matrix[2][1], this.matrix[2][2],
-        this.matrix[3][0], this.matrix[3][1], this.matrix[3][2]
+        this.matrix[2][0], this.matrix[2][1], this.matrix[2][2]
     );
 };
 
-Matrix4By3.prototype.sub = function(m) {
+Matrix3By3.prototype.sub = function(m) {
     this.add(m.copy().mult(-1));
 
     return this;
 };
 
-Matrix4By3.prod = function(m1, m2) {
+Matrix3By3.prod = function(m1, m2) {
     const matrix = Matrix.prod(m1, m2);
 
     let res = Matrix4By4.fromArray(matrix);
