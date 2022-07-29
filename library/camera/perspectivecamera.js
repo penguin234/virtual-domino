a1f2bcedec694afb66932e0d91babb41f6bd251f
penguin234 <56291718+penguin234@users.noreply.github.com>
2022-07-29 19:39:04 +0900

    Made Perspective Camera Frame

--- library/camera/perspectivecamera.js
+++ library/camera/perspectivecamera.js
@@ -4,3 +4,34 @@ function PerspectiveCamera(position, lookdirection, updirection, horizontalangle
     this.updirection = updirection;
     this.horizontalangle = horizontalangle;
 }
+
+function GetPositionAdjustTransform(camera) {
+    return new Translate(camera.position.x, camera.position.y, camera.position.z);
+}
+
+function GetDirectionAdjustTransform(camera) {
+
+}
+
+function GetConvertToPixelMatrix(camera) {
+    return new Matrix3By3(
+        1, 0, -0.5,
+        0, 1, -0.5,
+        0, 0, 1
+    );
+}
+
+PerspectiveCamera.prototype.GetProjectionTransform = function() {
+    const pos = GetPositionAdjustTransform(this);
+    const dir = GetDirectionAdjustTransform(this);
+    const coord = new Matrix4By3(
+        dir.read(0, 0), dir.read(0, 1), dir.read(0, 2),
+        dir.read(1, 0), dir.read(1, 1), dir.read(1, 2),
+        dir.read(2, 0), dir.read(2, 1), dir.read(2, 2),
+        pos.read(3, 0), pos.read(3, 1), pos.read(3, 2)
+    );
+
+    const pix = GetConvertToPixelMatrix(this);
+
+    return Applier.merge(coord, pix);
+}
