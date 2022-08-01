eb82db95a8f77ff1e59183c5d9c5864a16cb9df8
penguin234 <56291718+penguin234@users.noreply.github.com>
2022-08-01 15:58:32 +0900

    Added vector3d inner/outer product and tests

--- library/3d-elements/vector3d.js
+++ library/3d-elements/vector3d.js
@@ -51,3 +51,11 @@ Vector3D.prototype.normalize = function() {
 
     return this;
 };
+
+Vector3D.prototype.innerProduct = function(v) {
+    return this.x * v.x + this.y * v.y + this.z * v.z;
+}
+
+Vector3D.prototype.outerProduct = function(v) {
+    return new Vector3D(this.y * v.z - this.z * v.y, this.z * v.x - this.x - v.z, this.x * v.y - this.y * v.x);
+}
--- library/tests/test.vector3d.js
+++ library/tests/test.vector3d.js
@@ -17,7 +17,7 @@ function MakeApproximateCorrection(d) {
 describe('Vector3D', function() {
     const tolerance = 1 / 1000000;
     const correction = MakeApproximateCorrection(tolerance);
-    const isVectorsEqual = function(actual, expected) {
+    const isVectorEqual = function(actual, expected) {
         assert(CorrectVector3D(actual, expected, correction), actual.toString() + ' doesn\'t match ' + expected.toString())
     }
 
@@ -27,7 +27,7 @@ describe('Vector3D', function() {
             let v2 = new Vector3D(2, 2, 2);
             let expected = new Vector3D(3, 4, 5);
 
-            isVectorsEqual(v1.add(v2), expected);
+            isVectorEqual(v1.add(v2), expected);
         });
 
         it('add (-1, -4, 5) to (4.5, 3.2, 2.3) is (3.5, -0.8, 7.3)', function() {
@@ -35,7 +35,7 @@ describe('Vector3D', function() {
             let v2 = new Vector3D(-1, -4, 5);
             let expected = new Vector3D(3.5, -0.8, 7.3);
 
-            isVectorsEqual(v1.add(v2), expected);
+            isVectorEqual(v1.add(v2), expected);
         });
     });
 
@@ -45,7 +45,7 @@ describe('Vector3D', function() {
             let c = 3;
             let expected = new Vector3D(3, 6, 9);
 
-            isVectorsEqual(v1.mult(c), expected);
+            isVectorEqual(v1.mult(c), expected);
         });
 
         it('multiply 0.7 to (2, 2, -2) is (1.4, 1.4, -1.4)', function() {
@@ -53,7 +53,7 @@ describe('Vector3D', function() {
             let c = 0.7;
             let expected = new Vector3D(1.4, 1.4, -1.4);
 
-            isVectorsEqual(v1.mult(c), expected);
+            isVectorEqual(v1.mult(c), expected);
         });
     });
 
@@ -92,28 +92,96 @@ describe('Vector3D', function() {
             let v1 = new Vector3D(2, 0, 0);
             let expected = new Vector3D(1, 0, 0);
 
-            isVectorsEqual(v1.normalize(), expected);
+            isVectorEqual(v1.normalize(), expected);
         });
 
         it('normalize (0, -4, 0) is (0, -1, 0)', function() {
             let v1 = new Vector3D(0, -4, 0);
             let expected = new Vector3D(0, -1, 0);
 
-            isVectorsEqual(v1.normalize(), expected);
+            isVectorEqual(v1.normalize(), expected);
         });
 
         it('normalize (0, 0, -0.3) is (0, 0, -1)', function() {
             let v1 = new Vector3D(0, 0, -0.3);
             let expected = new Vector3D(0, 0, -1);
 
-            isVectorsEqual(v1.normalize(), expected);
+            isVectorEqual(v1.normalize(), expected);
         });
 
         it('normalize (1, -1, 1) is (sqrt(1/3), -sqrt(1/3), sqrt(1/3))', function() {
             let v1 = new Vector3D(1, -1, 1);
             let expected = new Vector3D(Math.sqrt(1 / 3), -Math.sqrt(1 / 3), Math.sqrt(1 / 3));
 
-            isVectorsEqual(v1.normalize(), expected);
+            isVectorEqual(v1.normalize(), expected);
+        });
+    });
+
+    describe('#innerProduct()', function() {
+        it('(1, 0, 0) inner product (2, 4, 6) is 2', function() {
+            let v1 = new Vector3D(1, 0, 0);
+            let v2 = new Vector3D(2, 4, 6);
+            let expected = 2;
+
+            v1.innerProduct(v2).should.equal(expected);
+        });
+
+        it('(0, 1, 0) inner product (2, 4, 6) is 4', function() {
+            let v1 = new Vector3D(0, 1, 0);
+            let v2 = new Vector3D(2, 4, 6);
+            let expected = 4;
+
+            v1.innerProduct(v2).should.equal(expected);
+        });
+
+        it('(0, 0, 1) inner product (2, 4, 6) is 6', function() {
+            let v1 = new Vector3D(0, 0, 1);
+            let v2 = new Vector3D(2, 4, 6);
+            let expected = 6;
+
+            v1.innerProduct(v2).should.equal(expected);
+        });
+
+        it('(1, 2, 3) inner product (4, 5, 6) is 32', function() {
+            let v1 = new Vector3D(1, 2, 3);
+            let v2 = new Vector3D(4, 5, 6);
+            let expected = 32;
+
+            v1.innerProduct(v2).should.equal(expected);
+        });
+    });
+
+    describe('#outerProduct()', function() {
+        it('(1, 0, 0) cross (0, 1, 0) is (0, 0, 1)', function() {
+            let v1 = new Vector3D(1, 0 ,0);
+            let v2 = new Vector3D(0, 1, 0);
+            let expected = new Vector3D(0, 0, 1);
+
+            isVectorEqual(v1.outerProduct(v2), expected);
+        });
+
+        it('(1, 0, 0) cross (0, 0, 1) is (0, -1, 0)', function() {
+            let v1 = new Vector3D(1, 0 ,0);
+            let v2 = new Vector3D(0, 0, 1);
+            let expected = new Vector3D(0, -1, 0);
+
+            isVectorEqual(v1.outerProduct(v2), expected);
+        });
+
+        it('(0, 1, 0) cross (0, 0, 1) is (1, 0, 0)', function() {
+            let v1 = new Vector3D(0, 1 ,0);
+            let v2 = new Vector3D(0, 0, 1);
+            let expected = new Vector3D(1, 0, 0);
+
+            isVectorEqual(v1.outerProduct(v2), expected);
+        });
+
+        it('(1, 0, 0) cross (0, 1, 0) is (0, 0, 1)', function() {
+            let v1 = new Vector3D(1, 0 ,0);
+            let v2 = new Vector3D(0, 1, 0);
+            let expected = new Vector3D(0, 0, 1);
+
+            isVectorEqual(v1.outerProduct(v2), expected);
         });
     });
 });
