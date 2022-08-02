4127a7cbdf55774d1a6cbbee675d854fbee30b57
penguin234 <56291718+penguin234@users.noreply.github.com>
2022-08-02 20:48:51 +0900

    Added Point2D to render projected points

---
+++ library/3d-elements/point2d.js
@@ -0,0 +1,17 @@
+function Point2D(x, y, d) {
+    this.x = x;
+    this.y = y;
+    this.d = d;
+}
+
+Point2D.NormalizeProjectedPoint = function(p) {
+    return new Point2D(p.x / p.z, p.y / p.z, p.z);
+};
+
+Point2D.prototype.toString = function() {
+    return '(' + String(this.x) + ', ' + String(this.y) + ')  depth: ' + String(this.d);
+};
+
+Point2D.prototype.log = function() {
+    console.log(this.toString());
+};
