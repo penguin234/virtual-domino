d6f5b952f5da6bc00641e004516bec262dfe1aeb
penguin234 <56291718+penguin234@users.noreply.github.com>
2022-08-11 21:17:46 +0900

    Customized vue struct

---
+++ public/src/views/HomeView.vue
@@ -0,0 +1,7 @@
+<script setup>
+</script>
+
+<template>
+  <h2> Home </h2>
+  <a href="wiki"> Wiki </a>
+</template>
---
+++ public/src/views/WikiView.vue
@@ -0,0 +1,7 @@
+<script setup>
+import CircularController from '@/components/CircularController.vue'
+</script>
+
+<template>
+  <CircularController />
+</template>
