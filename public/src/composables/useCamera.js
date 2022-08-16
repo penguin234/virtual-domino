904a8e78963556daebb757912099633dbfb03c5c
penguin234 <56291718+penguin234@users.noreply.github.com>
2022-08-16 14:22:33 +0900

    Looking at build issue

--- public/src/composables/useCamera.js
+++ public/src/composables/useCamera.js
@@ -3,16 +3,26 @@ import { ref, isRef, unref, watchEffect } from 'vue'
 import PerspectiveCamera from '../library/camera/perspectivecamera';
 
 export function useCamera(position, lookDirection, upDirection, horizontalAngle, viewboxWidth) {
+    console.log('a')
     const camera = new PerspectiveCamera(null, null, null, null, null);
+    console.log('b')
     const projection = ref(null);
-    const properUpdirection = ref(null)
+    console.log('c')
+    const properUpdirection = ref(null);
+    console.log('d')
 
     function moveCamera() {
+        console.log('e')
         camera.position = unref(position);
+        console.log('f')
         camera.lookdirection = unref(lookDirection);
+        console.log('g')
         properUpdirection.value = camera.GetProperUpdirection(unref(upDirection));
+        console.log('h')
         camera.updirection = properUpdirection.value
+        console.log('i')
         camera.horizontalangle = unref(horizontalAngle);
+        console.log('j')
         camera.viewboxwidth = unref(viewboxWidth);
 
         console.log(camera)
@@ -21,12 +31,16 @@ export function useCamera(position, lookDirection, upDirection, horizontalAngle,
         projection.value = camera.GetProjectionTransform();
     }
 
+    console.log('k')
     if (isRef(position) || isRef(lookDirection) || isRef(upDirection) || isRef(horizontalAngle) || isRef(viewboxWidth)) {
+        console.log('l')
         watchEffect(moveCamera);
     }
     else {
+        console.log('m')
         moveCamera();
     }
+    console.log('n')
 
     return { projection, properUpdirection };
 }
